e4f9a969fc57848deaaf9d1c2ce8033df74c95b0
Rolando Antón <rolando@anton.sh>
2020-06-29 14:34:21 -0500

    Update PH_DDL_Server_Level_Events.sql

--- PH_DDL_Server_Level_Events.sql
+++ PH_DDL_Server_Level_Events.sql
@@ -1,14 +1,9 @@
 CREATE TRIGGER PH_DDL_Server_Level_Events
 ON ALL SERVER
  
-FOR DDL_ENDPOINT_EVENTS, DDL_LOGIN_EVENTS, DDL_GDR_SERVER_EVENTS, DDL_AUTHORIZATION_SERVER_EVENTS, 
-CREATE_DATABASE, ALTER_DATABASE, DROP_DATABASE
-/**FOR DDL_SERVER_LEVEL_EVENTS**/
-AS
-DECLARE @eventData AS XML;
+FOR DDL_ENDPOINT_EVENTS, DDL_LOGIN_EVENTS, DDL_GDR_SERVER_EVENTS, DDL_AUTHORIZATION_SERVER_EVENTS, CREATE_DATABASE, ALTER_DATABASE, DROP_DATABASE AS DECLARE @eventData AS XML;
 SET @eventData = EVENTDATA();
-/**declare @eventData as XML;
-set @eventData = EVENTDATA();**/
+
 insert into PH_Events.dbo.DDLEvents(EventTime, EventType, SPID, ServerName, LoginName, ObjectName, ObjectType, SchemaName, DatabaseName, CommandText, XMLEvent)
 values(cast(@eventData.query('data(//PostTime)') as varchar(64)),
        cast(@eventData.query('data(//EventType)') as varchar(128)),
@@ -20,5 +15,4 @@ values(cast(@eventData.query('data(//PostTime)') as varchar(64)),
        cast(@eventData.query('data(//SchemaName)') as varchar(128)),
        cast(@eventData.query('data(//DatabaseName)') as varchar(64)),
        cast(@eventData.query('data(//TSQLCommand/CommandText)') AS NVARCHAR(MAX)),
-       /**  DB_NAME(),**/
        @eventData); 
