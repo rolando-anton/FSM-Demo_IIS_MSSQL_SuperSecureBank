82e6a969c5a267f48f6aaf8f5b2fcad32eacd868
Rolando Antón <rolando@anton.sh>
2020-06-29 22:37:20 -0500

    Update and rename new-ro-user.sql to new-ro-users.sql

--- new-ro-user.sql
+++
@@ -1,6 +0,0 @@
-EXEC SP_ADDLOGIN 'AOPerfLogin', 'ProspectHills!', 'master';
-EXEC SP_ADDROLE 'AOPerfRole';
-EXEC SP_ADDUSER 'AOPerfLogin', 'AOPerfUser', 'AOPerfRole';
-GRANT VIEW SERVER STATE TO AOPerfLogin;
-GRANT SELECT ON dbo.sysperfinfo TO AOPerfRole;
-GRANT EXEC on xp_readerrorlog to AOPerfRole
